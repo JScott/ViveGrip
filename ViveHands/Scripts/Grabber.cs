cec0b3393cbeb015741f84b7a9de406138910725
JScott <jvscott@gmail.com>
2016-04-26 22:21:24 -0700

    Selecting items

---
+++ ViveHands/Scripts/Grabber.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grabber : MonoBehaviour {
+	public Vector3 anchor = new Vector3(0,0,1f);
+  public float grabRadius = 0.5f;
+  public Shader outline;
+  private Shader oldShader;
+  // TODO: set outline in script with Shader.Find
+  private GameObject currentObject;
+  private GrabberSphere grabberSphere;
+
+  void Start () {
+    anchor = Vector3.Scale(anchor, transform.GetComponent<Renderer>().bounds.size/2);
+    GameObject grabberObject = InstantiateGrabberObject();
+    grabberSphere = grabberObject.AddComponent<GrabberSphere>();
+    grabberSphere.radius = grabRadius;
+	}
+
+  GameObject InstantiateGrabberObject() {
+    GameObject grabberObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    grabberObject.GetComponent<Renderer>().enabled = false;
+    grabberObject.transform.localScale = new Vector3(grabRadius, grabRadius, grabRadius);
+    grabberObject.transform.SetParent(transform.parent);
+    grabberObject.transform.localPosition = anchor;
+    grabberObject.name = "GrabberSphere";
+    return grabberObject;
+  }
+
+	void Update () {
+    GameObject touchedObject = grabberSphere.ClosestObject();
+    if (touchedObject != currentObject) {
+      if (currentObject != null) {
+        currentObject.GetComponent<Renderer>().material.shader = oldShader;
+      }
+      currentObject = touchedObject;
+      if (touchedObject != null) {
+        oldShader = currentObject.GetComponent<Renderer>().material.shader;
+        currentObject.GetComponent<Renderer>().material.shader = outline;
+      }
+    }
+	}
+}
---
+++ ViveHands/Scripts/GrabberSphere.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GrabberSphere : MonoBehaviour {
+  public float radius = 1f;
+  private List<GameObject> collidingObjects = new List<GameObject>();
+
+	void Start () {
+    GetComponent<SphereCollider>().isTrigger = true;
+	}
+
+  void OnTriggerEnter(Collider other) {
+    collidingObjects.Add(other.gameObject);
+    Debug.Log("Added "+other.gameObject.name);
+  }
+
+  void OnTriggerExit(Collider other) {
+    collidingObjects.Remove(other.gameObject);
+    Debug.Log("Removed "+other.gameObject.name);
+  }
+
+  public GameObject ClosestObject() {
+    float closestDistance = radius + 1f;
+    GameObject touchedObject = null;
+    foreach (GameObject gameObject in collidingObjects) {
+      float distance = Vector3.Distance(transform.position, gameObject.transform.position);
+      if (distance < closestDistance) {
+        touchedObject = gameObject;
+        closestDistance = distance;
+      }
+    }
+    return touchedObject;
+  }
+}
