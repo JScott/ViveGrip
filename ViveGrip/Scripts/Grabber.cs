bb56ff873c2f78e840f8cd481eb9e03553a2f610
JScott <jvscott@gmail.com>
2016-04-30 10:10:54 -0700

    A lot of fixing things and a lot of breaking things

--- ViveGrip/Scripts/Grabber.cs
+++ ViveGrip/Scripts/Grabber.cs
@@ -17,6 +17,8 @@ public class Grabber : MonoBehaviour {
   private bool anchored = false;
   private SteamVR_Controller.Device device;
 
+  // TODO: only let one hand grab at a time?
+
   void Start() {
     GameObject grabberObject = InstantiateGrabberObjectOn(grabberJoint);
     grabberSphere = grabberObject.AddComponent<GrabberSphere>();
@@ -25,7 +27,7 @@ public class Grabber : MonoBehaviour {
 	}
 
   void Update() {
-    device = GetDevice(); // TODO: what happens with both controllers?
+    device = GetDevice();
     GameObject touchedObject = grabberSphere.ClosestObject();
     HandleGrabbing(touchedObject);
     UpdateHighlighting(touchedObject);
@@ -33,6 +35,7 @@ public class Grabber : MonoBehaviour {
   }
 
   void HandleGrabbing(GameObject touchedObject) {
+    if (device == null) { return; }
     bool shouldConnect = !SomethingHeld() && touchedObject != null && device.GetTouchDown(gripInput);
     if (shouldConnect) {
       CreateConnectionTo(touchedObject.GetComponent<Rigidbody>());
@@ -64,7 +67,10 @@ public class Grabber : MonoBehaviour {
     }
     highlightedObject = touchedObject;
     if (touchedObject != null) {
-      oldShader = highlightedObject.GetComponent<Renderer>().material.shader;
+      Shader currentShader = highlightedObject.GetComponent<Renderer>().material.shader;
+      if (currentShader != outlineShader) {
+        oldShader = currentShader;
+      }
       highlightedObject.GetComponent<Renderer>().material.shader = outlineShader;
       highlightedObject.GetComponent<Renderer>().material.SetFloat("_Outline", 0.0005f);
       highlightedObject.GetComponent<Renderer>().material.SetColor("_OutlineColor", outlineColor);
@@ -73,6 +79,7 @@ public class Grabber : MonoBehaviour {
 
   SteamVR_Controller.Device GetDevice() {
     SteamVR_TrackedObject trackedObject = transform.parent.GetComponent<SteamVR_TrackedObject>();
+    if (trackedObject == null) { return null };
     return SteamVR_Controller.Input((int)trackedObject.index);
   }
 
@@ -104,10 +111,6 @@ public class Grabber : MonoBehaviour {
     return transform.position + transform.TransformVector(defaultAnchor);
   }
 
-  Vector3 AnchorDefaultLocalPosition() {
-    return Vector3.Scale(defaultAnchor, transform.localScale);
-  }
-
   Vector3 GrabbableAnchorWorldPosition(Transform desiredObjectTransform) {
     Vector3 anchor = desiredObjectTransform.GetComponent<Grabbable>().anchor;
     return desiredObjectTransform.position + desiredObjectTransform.TransformVector(anchor);
@@ -131,8 +134,9 @@ public class Grabber : MonoBehaviour {
       grabberObject.GetComponent<Renderer>().enabled = false;
     }
     grabberObject.transform.localScale = new Vector3(grabRadius, grabRadius, grabRadius);
+    grabberObject.transform.position = AnchorDefaultWorldPosition();
     grabberObject.transform.SetParent(transform.parent);
-    grabberObject.transform.localPosition = AnchorDefaultLocalPosition();
+    Debug.DrawLine(grabberObject.transform.position, transform.parent.position, Color.red, 20, false);
     grabberObject.name = "Grabber Sphere";
     return grabberObject;
   }
