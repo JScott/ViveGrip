7eb70e72c36fe93fb34db540cbf732f1934c861d
JScott <jvscott@gmail.com>
2016-05-02 23:29:26 -0700

    Cleaning up the joint factory

--- ViveGrip/Scripts/Internal/ViveGrip_JointFactory.cs
+++ ViveGrip/Scripts/Internal/ViveGrip_JointFactory.cs
@@ -4,28 +4,15 @@ using System.Collections;
 public static class ViveGrip_JointFactory {
   public static ConfigurableJoint JointToConnect(GameObject jointObject, Rigidbody desiredObject, Vector3 offset, Quaternion desiredRotation) {
     ConfigurableJoint joint = jointObject.AddComponent<ConfigurableJoint>();
-
     ViveGrip_JointFactory.SetLinearDrive(joint, desiredObject.mass);
-    ViveGrip_JointFactory.ConfigureBase(joint, offset);
-
     ViveGrip_JointFactory.SetAngularDrive(joint, desiredObject.mass);
-    Quaternion currentRotation = desiredObject.transform.rotation;
-    joint.SetTargetRotationLocal(desiredRotation, currentRotation);
-
-    joint.connectedBody = desiredObject;
-    joint.connectedBody.useGravity = false;
+    ViveGrip_JointFactory.ConfigureAnchor(joint, offset);
+    ViveGrip_JointFactory.ConfigureRotation(joint, desiredObject, desiredRotation);
+    ViveGrip_JointFactory.Attach(joint, desiredObject);
     return joint;
   }
 
-  private static void ConfigureBase(ConfigurableJoint joint, Vector3 offset) {
-    ConfigurableJointMotion linearMotion = ConfigurableJointMotion.Free;
-    ConfigurableJointMotion angularMotion = ConfigurableJointMotion.Free;
-    joint.xMotion = linearMotion;
-    joint.yMotion = linearMotion;
-    joint.zMotion = linearMotion;
-    joint.angularXMotion = angularMotion;
-    joint.angularYMotion = angularMotion;
-    joint.angularZMotion = angularMotion;
+  private static void ConfigureAnchor(ConfigurableJoint joint, Vector3 offset) {
     joint.autoConfigureConnectedAnchor = false;
     joint.connectedAnchor = offset;
   }
@@ -47,6 +34,11 @@ public static class ViveGrip_JointFactory {
     joint.zDrive = jointDrive;
   }
 
+  private static void ConfigureRotation(ConfigurableJoint joint, Rigidbody desiredObject, Quaternion desiredRotation) {
+    Quaternion currentRotation = desiredObject.transform.rotation;
+    joint.SetTargetRotationLocal(desiredRotation, currentRotation);
+  }
+
   private static void SetAngularDrive(ConfigurableJoint joint, float mass) {
     float gripStrength = 3000f * mass;
     float gripSpeed = 10f * mass;
@@ -59,5 +51,10 @@ public static class ViveGrip_JointFactory {
     jointDrive.positionSpring = gripStrength;
     jointDrive.positionDamper = gripSpeed;
     joint.angularXDrive = jointDrive;
+  }
+
+  private static void Attach(ConfigurableJoint joint, Rigidbody desiredObject) {
+    joint.connectedBody = desiredObject;
+    joint.connectedBody.useGravity = false;
   }
 }
