e7674ce9167f4234aa0b572bb785609c9555261b
JScott <jvscott@gmail.com>
2016-06-27 23:07:03 -0700

    Added a door. The door is messed up.

---
+++ Assets/ViveGrip/Examples/Scripts/ViveGripExample_DoorHandle.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveGripExample_DoorHandle : MonoBehaviour {
+	void Start () {
+    Physics.IgnoreCollision(GameObject.Find("Door").GetComponent<Collider>(), GetComponent<Collider>());
+	}
+}
