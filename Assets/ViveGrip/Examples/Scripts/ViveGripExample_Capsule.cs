7950cb6179b24c54b6bfcd53ad7b436deba8e019
JScott <jvscott@gmail.com>
2016-07-16 10:57:19 -0700

    Improved lighting

---
+++ Assets/ViveGrip/Examples/Scripts/ViveGripExample_Capsule.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveGripExample_Capsule : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
