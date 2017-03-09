ac85523de980e2cca5c71331091e9869031b461a
JScott <jvscott@gmail.com>
2017-03-09 12:18:43 -0800

    Only look for the parent in Compound Colliders

--- Assets/ViveGrip/Scripts/Internal/ViveGrip_TouchDetection.cs
+++ Assets/ViveGrip/Scripts/Internal/ViveGrip_TouchDetection.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 
 public class ViveGrip_TouchDetection : MonoBehaviour {
-  public float radius = 1f;
   private List<GameObject> collidingObjects = new List<GameObject>();
 
   void Start () {
@@ -35,12 +34,43 @@ public class ViveGrip_TouchDetection : MonoBehaviour {
 
   GameObject ActiveViveGripObject(GameObject gameObject) {
     if (gameObject == null) { return null; } // Happens with Destroy() sometimes
-    ViveGrip_Grabbable grabbable = gameObject.GetComponentInParent<ViveGrip_Grabbable>();
-    bool validGrabbable = grabbable != null && grabbable.enabled;
-    if (validGrabbable) return grabbable.gameObject;
-    ViveGrip_Interactable interactable = gameObject.GetComponentInParent<ViveGrip_Interactable>();
-    bool validInteractable = interactable != null && interactable.enabled;
-    if (validInteractable) return interactable.gameObject;
+    ViveGrip_Grabbable grabbable = FindAnyValid<ViveGrip_Grabbable>(gameObject);
+    if (grabbable != null) { return grabbable.gameObject; }
+    ViveGrip_Interactable interactable = FindAnyValid<ViveGrip_Interactable>(gameObject);
+    if (interactable != null) { return interactable.gameObject; }
     return null;
+  }
+
+  T FindAnyValid<T>(GameObject gameObject) where T : class {
+    // var component = gameObject.GetComponent<T>();
+    // if (component != null && Enabled<T>(component)) { return component; }
+    // Transform parent = gameObject.transform.parent;
+    // if (parent != null) {
+    //   component = parent.GetComponent<T>();
+    //   if (component != null && Enabled<T>(component)) { return component; }
+    // }
+    // return null;
+    T component = FindValid<T>(gameObject.transform);
+    if (component == null) {
+      component = FindValid<T>(gameObject.transform.parent);
+    }
+    return component;
+  }
+
+  T FindValid<T>(Transform transform) where T : class {
+    if (transform == null) { return null; }
+    T component = transform.GetComponent<T>();
+    if (component != null && Enabled<T>(component)) { return component; }
+    return null;
+  }
+
+  bool Enabled<T>(T component) {
+    if (component is ViveGrip_Grabbable) {
+      return (component as ViveGrip_Grabbable).enabled;
+    }
+    if (component is ViveGrip_Interactable) {
+      return (component as ViveGrip_Interactable).enabled;
+    }
+    return false;
   }
 }
